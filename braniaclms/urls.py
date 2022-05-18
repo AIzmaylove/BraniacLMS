1d186e60ed78303e5e7c73c9d453b1cd5bffe5c8
A_Izmaylove <its8my8life@gmail.com>
2022-05-18 21:39:00 +0300

    homework lesson_3

--- braniaclms/urls.py
+++ braniaclms/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mainapp import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('',include('mainapp.urls'))
+    path('', RedirectView.as_view(url='mainapp/')),
+    path('mainapp/',include('mainapp.urls', namespace='mainapp')),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,35 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import TemplateView
+from datetime import datetime
 
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['contacts'] = [
+            {
+                'mapa': 'https://yandex.ru/map-widget/v1/-/CCUAZHcrhA',
+                'city': 'Санкт-Петербург',
+                'phone': '+7-999-111-11-11',
+                'adress': 'территория Петропавловская крепость, 3Ж',
+                'email': 'geekbrains@spb.ru'
+            },{
+                'mapa': 'https://yandex.ru/map-widget/v1/-/CCUAZHX3xB',
+                'city': 'Казань',
+                'phone': '+7-999-222-22-22',
+                'adress':'территория Кремль, 11, Казань, Республика Татарстан, Россия',
+                'email': 'geekbrains@kzn.ru'
+            },{
+                'mapa': 'https://yandex.ru/map-widget/v1/-/CCUAZHh9kD',
+                'city': 'Москва',
+                'phone': '+7-999-333-33-33',
+                'adress': 'Красная площадь, 7, Москва, Россия',
+                'email': 'geekbrains@msk.ru'
+            }
+            
+        ]
+        return context_data
+
 
 class CoursesListView(TemplateView):
     template_name = 'mainapp/courses_list.html'
@@ -19,3 +45,31 @@ class LoginView(TemplateView):
     
 class NewsView(TemplateView):
     template_name = 'mainapp/news.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = [
+            {
+                'title': 'Новость раз',
+                'preview': 'Превью для новости раз',
+                'date': datetime.now()
+            },{
+                'title': 'Новость два',
+                'preview': 'Превью для новости два',
+                'date': datetime.now()
+            },{
+                'title': 'Новость три',
+                'preview': 'Превью для новости три',
+                'date': datetime.now()
+            },{
+                'title': 'Новость четыре',
+                'preview': 'Превью для новости четрые',
+                'date': datetime.now()
+            },{
+                'title': 'Новость пять',
+                'preview': 'Превью для новости пять',
+                'date': datetime.now()
+            }
+        ]
+        
+        return context_data
